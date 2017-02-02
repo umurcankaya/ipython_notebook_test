248a5d748702dcae92d1cc2ee679012613c595d9
Umur Can Kaya <umurcan.kaya@gmail.com>
2017-02-02 04:10:36 +0200

    Add files via upload

---
+++ curve_fit.py
@@ -0,0 +1,21 @@
+import numpy as np
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+
+
+def func(x, a, b, c):
+    return a * np.exp(-b * x) + c
+
+xdata = np.linspace(0, 4, 50)
+y = func(xdata, 2.5, 1.3, 0.5)
+ydata = y + 0.2 * np.random.normal(size=len(xdata))
+
+
+popt, pcov = curve_fit(func, xdata, ydata)
+
+yfit = func(xdata, popt[0], popt[1], popt[2])
+
+plt.scatter(xdata, ydata, color='red')
+plt.plot(xdata, yfit)
+plt.show()
+
